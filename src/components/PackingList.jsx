804cd87080ef1d66c046e744313f80eb8a913bd1
Thanh Hieu <thanhhieu8304@gmail.com>
2025-08-25 16:10:27 +0700

    Implement sorting functionality in PackingList component for improved item organization

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Item from "./Item";
 
 function PackingList({ items, onUpdateItem, onDeleteItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  switch (sortBy) {
+    case "input": {
+      sortedItems = items;
+      break;
+    }
+    case "description": {
+      // We don't want to mutate the original items --> use slice
+      sortedItems = items.slice().sort(
+        (item1, item2) => item1.description.localeCompare(item2.description) // Compare string
+      );
+      break;
+    }
+    case "packed": {
+      // We don't want to mutate the original items --> use slice
+      sortedItems = items
+        .slice()
+        .sort((item1, item2) => Number(item1.packed) - Number(item2.packed)); // Compare boolean
+      break;
+    }
+    default: {
+      sortedItems = items;
+    }
+  }
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -16,11 +43,15 @@ function PackingList({ items, onUpdateItem, onDeleteItem }) {
         ))}
       </ul>
       <div className="actions">
-        <select name="cars" id="cars">
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="opel">Opel</option>
-          <option value="audi">Audi</option>
+        <select
+          name="sortingCriteria"
+          id="sortingCriteria"
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
         </select>
         <button>Clear list</button>
       </div>
