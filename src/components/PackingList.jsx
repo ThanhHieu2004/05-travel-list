e8844f08ade4fcfc12ee8b269db5e6867a309a6f
Thanh Hieu <thanhhieu8304@gmail.com>
2025-08-25 10:06:17 +0700

    Initial commit

---
+++ src/components/PackingList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import Item from "./Item";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+      <div className="actions">
+        <select name="cars" id="cars">
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select>
+        <button>Clear list</button>
+      </div>
+    </div>
+  );
+}
+
+export default PackingList;
