26378ddde07df8ba9408d061e3c0031bb81881d5
Thanh Hieu <thanhhieu8304@gmail.com>
2025-08-25 15:54:05 +0700

    Refactor Stats component to improve packed items calculation and enhance display messages.

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 
 function Stats({ items }) {
+  // Early return
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start adding items to your packing list. 🌟</em>
+      </footer>
+    );
+
   // Derived State
   const itemsCount = items.length;
-  const packedItems = items.filter((item) => item.packed === true).length;
+  const packedItems = items.filter((item) => item.packed).length;
   const finishedPercentage =
     Math.round((packedItems / itemsCount) * 10000) / 100;
 
   return (
     <footer className="stats">
       <em>
-        🐉 You have {itemsCount} items on your list, and you already packed{" "}
-        {packedItems} ({finishedPercentage}%)
+        {finishedPercentage === 100
+          ? "You got everything. Ready to go! 🐋"
+          : ` 🐉 You have ${itemsCount} items on your list, and you already packed
+        ${packedItems} (${finishedPercentage}%)`}
       </em>
     </footer>
   );
